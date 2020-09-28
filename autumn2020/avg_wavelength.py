c3739f052fa79c9fac45e66d1c00de18289e07d4
LeonPhy <leon140497@aol.com>
2020-09-28 09:10:14 +0200

    Add existing file

---
+++ autumn2020/avg_wavelength.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Calculates the average wavelengths of the passbands.dat (bachelor/passbands) in Angstrom
+"""
+
+
+from astropy.io import ascii
+import numpy as np
+import json
+from glob import glob
+
+def main():
+    colors = glob('data/bachelor/passbands/*.dat')
+    #Sorting them by alphabet
+    colors.sort()
+    #Manually sorted to fit the order of the data
+    idx = [3,4,5,6,8,7,10,9,0,1,2,11,12]
+    sort = colors.copy()
+
+    for i,j in enumerate(idx):
+        colors[i] = sort[j]
+
+    mean = []
+
+    #Read out passband wavelengths and strength and calculate mean
+    print('Band               Mean Wavelength')
+    print('----------------------------------')
+    for i, b in enumerate(colors):
+        x, y = [], []
+        data = ascii.read(b)
+        for row in range(len(data)):
+            xt, yt = data[row]
+            x.append(xt)
+            y.append(yt)
+
+        x = np.array(x)
+        y = np.array(y)
+        mean.append(np.sum(x*y)/np.sum(y))
+        print(b[24:-4], mean[i])
+
+    file = 'mean_wavelengths.json'
+    with open(file, 'w') as f:
+        json.dump(mean, f)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ autumn2020/mock_data.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generate a mock data catalogue so we can test the method and see if we can recover these values.
+"""
+
+import h5py
+import json
+import numpy as np
+import silence_tensorflow.auto
+import tensorflow as tf
+
+
+def read_out(path):
+    # read out the existing Test file supplied by Greg's Paper
+    with h5py.File(path, 'r') as f:
+        d = f['data'][:]
+        r = f['r_fit'][:]
+
+    return d, r
+
+
+def save(path, d, r, R, dr, dR):
+    # save the mock data set with the used R, E, dR, dE
+    with h5py.File(path, 'w') as f:
+        f.create_dataset('/data', data=d, chunks=True, compression='gzip', compression_opts=3)
+        f.create_dataset('/r_fit', data=r, chunks=True, compression='gzip', compression_opts=3)
+        f.create_dataset('/R', data=R, chunks=True, compression='gzip', compression_opts=3)
+        f.create_dataset('/dr_fit', data=dr, chunks=True, compression='gzip', compression_opts=3)
+        f.create_dataset('/dR', data=dR, chunks=True, compression='gzip', compression_opts=3)
+
+def guess_dR(R):
+    # read out the mean wavelengths of the 13 filters
+    with open('mean_wavelengths.json', 'r') as f:
+        mean = json.load(f)
+
+    x = np.array(mean)
+
+    # calculate the offset of the linear scaling to garnatuee it is perpendicular to R
+    b = np.sum(x*R)/np.sum(R)
+    y = x[:] - b[None]
+
+    # norm it to 1
+    y /= np.dot(y,y)**0.5
+
+    return y
+
+
+def predict_M(theta, nn):
+    # define the partial Neural Network that predicts only BM
+    inputs = nn.get_layer('theta').input
+    outputs = nn.get_layer('BM').output
+    BM_model = tf.keras.Model(inputs, outputs)
+
+    # return the prediction of BM
+    return BM_model(theta).numpy()
+
+
+def predict_R(theta, nn):
+    # define the partial Neural Network that predicts only R
+    inputs = nn.get_layer('theta').input
+    outputs = nn.get_layer('R').output
+    R_model = tf.keras.Model(inputs, outputs)
+
+    # return the prediction of R
+    return R_model(theta).numpy()
+
+
+def mock_m(d, r, nn, seed = 14, max = 0.01):
+    """
+    Calculates a mock magnitude by predicting absolute magnitude and add Reddening as well as
+    Variation manually to create a mock magnitude.
+
+    Parameters
+    ----------
+    d: np.ndarray
+        Contains the readout data from the Greg's test data with parallax, theta and mag
+    r: np.array
+        Contains the reddening E for each star from Greg's test data
+    nn: keras.model
+        The neural Network supplied with Greg's test data
+    seed: int
+        RNG-Seed to reproduce dE values if needed
+    max: float
+        Max value for dE
+
+    Returns
+    -------
+    m: np.array
+        The mock magnitude we want to replace in the test data
+    BR: np.array
+        The general reddening vector we obtained from the Neural Network
+    dE: np.array
+        The reddening of each star of the test data set, which we randomly generated
+    BdR: np.array
+        The general reddening variation vector we obtained from guess_dR
+    """
+
+    # determine the amount of bands and stars measured, should be 13 bands and create the B-matrix
+    n_stars, n_bands = d['mag'].shape
+    B = np.identity(n_bands, dtype='f4')
+    B[1:,0] = -1.
+    B_inv = B.copy()
+    B_inv[1:,0] = 1
+
+    # predict R and take the mean to get a general reddening vector and transform it into mag/color
+    R_all = predict_R(d['atm_param_p'], nn)
+    R = np.mean(R_all, axis=0)
+    BR = np.einsum('ij,j->i',B,R)
+
+    # predict m from theta and take parallax and reddening into account for m
+    m = predict_M(d['atm_param_p'], nn)
+    m = np.einsum('ij,nj->ni',B_inv,m)
+    m[:,:] += (10. - 5.*np.log10(d['parallax']))[:,None]
+    m[:,:] += r[:,None] * R[None,:]
+
+    # guess BdR and generate random dE to add them to the mock m
+    dR = guess_dR(R)
+    BdR = np.einsum('ij,j->i',B,dR)
+    dE = max*np.random.default_rng(seed).random((n_stars,))
+    m[:,:] += dE[:,None] * dR[None,:]
+
+    return m, R, dE, dR
+
+
+def main():
+    # the folder location where the datasets are saved
+    dir = 'data/variation/'
+    # the actual file names
+    base = dir + 'green2020_test_data_small.h5'
+    mock = dir + 'mock_data.h5'
+    file = dir + 'green2020_nn_model.h5'
+
+    # load the Neural Network and read out the values from the test dataset
+    nn_model = tf.keras.models.load_model(file)
+    d, r = read_out(base)
+
+    #set up a mask to filter bad parallaxes for our mock data
+    err_over_plx = d['parallax_err']/d['parallax']
+    idx = ~(
+          (err_over_plx > 0.2)
+        | (d['parallax'] < 1.e-8)
+        | ~np.isfinite(d['parallax'])
+        | ~np.isfinite(d['parallax_err'])
+            )
+
+    d = d[idx]
+    r = r[idx]
+
+    # calculate R, dR and dE and the resulting new m
+    m, R, dr, dR = mock_m(d, r, nn_model)
+
+    # replace the observed magnitude with out calculated and save as mock_data.h5
+    d['mag'] = m
+    save(mock, d, r, R, dr, dR)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
